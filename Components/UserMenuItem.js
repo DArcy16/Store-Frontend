732171175a357ab1132c0c7617835ec56973546e
D'Arcy <walterluke1612@gmail.com>
2023-01-04 20:10:37 +0630

    final

--- Components/UserMenuItem.js
+++ Components/UserMenuItem.js
@@ -1,36 +1,28 @@
-import React from 'react';
-import { useUser } from '@auth0/nextjs-auth0/client';
+import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const UserMenuItem = () => {
-  const { user, error, isLoading } = useUser();
-  const router = useRouter();
+    const {user, error, isLoading} = useUser();
+    const router = useRouter();
 
-  // if not logged in
-  if (!user) {
+if(!user) {
     return (
-      <div>
-        <Link href='/api/auth/login' className='text-white btn'>
-          Login
-        </Link>
-      </div>
-    );
-  }
-  // if logged in
-  return (
-    <div
-      onClick={() => router.push('/profile')}
-      className='flex items-center gap-2 cursor-pointer'
-    >
-      <img
-        className='w-6 h-6 rounded-full'
-        src={user.picture}
-        alt={user.name}
-      />
-      <h3>{user.name}</h3>
+    <div>
+        <Link href='/api/auth/login' className='btn text-white'>Login</Link>
     </div>
-  );
-};
+);
+}  
+    
+    return (
+      
+      <div onClick={() => router.push('/profile')} className='flex items-center gap-2 cursor-pointer'>
+          <img className='rounded-full w-6 h-6' src={user.picture} alt={user.name} />
+          <h3>{user.name}</h3>
+      </div>
+    )
 
-export default UserMenuItem;
+}
+
+export default UserMenuItem
